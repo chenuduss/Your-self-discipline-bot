378703e0883a7e11aed113d6ea7fe3883863de31
arz <arz@weblock.su>
2024-10-10 22:14:05 +0300

    db schema update

--- db/r100.sql
+++ db/r100.sql
@@ -1,9 +1,21 @@
+CREATE TABLE sd_user (
+    id bigint NOT NULL PRIMARY KEY,
+    title varchar(200) NOT NULL   
+);
+
+CREATE TABLE chat (
+    id bigint NOT NULL PRIMARY KEY,
+    title varchar(200) NOT NULL   
+);
+
 CREATE TABLE self_contrib_record (
     user_id bigint NOT NULL,
     ts timestamp with time zone NOT NULL DEFAULT (current_timestamp AT TIME ZONE 'UTC'),
     chat_id bigint NOT NULL,
-    value int NOT NULL,
-    PRIMARY KEY (user_id, ts)
+    amount int NOT NULL,
+    PRIMARY KEY (user_id, ts),
+    FOREIGN KEY (sd_user) REFERENCES sd_user (id),
+    FOREIGN KEY (chat) REFERENCES chat (id)
 );
 
 CREATE INDEX idx_self_contrib_record_chat_id on self_contrib_record ("chat_id");

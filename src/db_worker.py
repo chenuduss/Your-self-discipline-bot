6ef9ff5c37c2d2975ebdd171bfaf6b9a33082cad
arz <arz@weblock.su>
2024-10-11 09:01:56 +0300

    push command and mystat command

--- src/db_worker.py
+++ src/db_worker.py
@@ -1,6 +1,8 @@
 import psycopg2
 import psycopg2.extras
 from psycopg2 import pool
+from datetime import datetime
+from ysdb_exception import YSDBException
 
 def ConnectionPool(function_to_decorate):    
     def wrapper(*args, **kwargs):
@@ -13,6 +15,11 @@ def ConnectionPool(function_to_decorate):
             obj.Pool.putconn(conn)     
         
     return wrapper
+
+class ChatRelatedUserSelfContrib:
+    def __init__(self, ts:datetime, amount:int):
+        self.TS = ts
+        self.Amount = amount
 
 class DbWorkerService:   
     def __init__(self, config:dict):
@@ -50,11 +57,46 @@ class DbWorkerService:
         ps_cursor = connection.cursor() 
         ps_cursor.execute("INSERT INTO self_contrib_record (user_id, chat_id, amount) VALUES (%s, %s, %s)", (user_id, chat_id, amount)) 
         connection.commit() 
+
+    @ConnectionPool    
+    def SelectLastUserSelfContribs(self, user_id:int, chat_id:int, limit:int,  connection=None) -> list[ChatRelatedUserSelfContrib]:
+        ps_cursor = connection.cursor()          
+        ps_cursor.execute("SELECT ts, amount FROM self_contrib_record WHERE user_id = %s AND chat_id = %s ORDER BY ts DESC LIMIT %s", (user_id, chat_id, limit))        
+        rows = ps_cursor.fetchall()        
+        result = []
+        for row in rows:
+            result.append(ChatRelatedUserSelfContrib(row[0], row[1]))
+
+        return result    
+
+
     
     @ConnectionPool    
-    def CalcAllUserStat(self, user_id:int, connection=None) -> None:
+    def GetAllAmountSum(self, user_id:int, connection=None) -> None:
         raise NotImplementedError("DbWorkerService.CalcAllUserStat")   
 
     @ConnectionPool    
-    def CalcUserStat(self, user_id:int, chat_id:int, connection=None) -> None:
-        raise NotImplementedError("DbWorkerService.CalcUserStat")     
+    def GetAmountSum(self, user_id:int, chat_id:int, start_ts:datetime, end_ts:datetime, connection=None) -> int:
+        ps_cursor = connection.cursor()          
+        ps_cursor.execute(
+            "SELECT sum(amount) FROM self_contrib_record WHERE user_id = %s AND chat_id = %s AND ts >= %s AND ts <= %s", 
+            (user_id, chat_id, start_ts, end_ts))        
+        rows = ps_cursor.fetchall()    
+        if len(rows) == 1:
+            return rows[0][0]
+        elif len(rows) > 1:
+            raise YSDBException("corrupted DB table")
+        return 0
+    
+    @ConnectionPool    
+    def GetChatAmountSum(self, chat_id:int, start_ts:datetime, end_ts:datetime, connection=None) -> int:
+        ps_cursor = connection.cursor()          
+        ps_cursor.execute(
+            "SELECT sum(amount) FROM self_contrib_record WHERE AND chat_id = %s AND ts >= %s AND ts <= %s", 
+            (chat_id, start_ts, end_ts))        
+        rows = ps_cursor.fetchall()    
+        if len(rows) == 1:
+            return rows[0][0]
+        elif len(rows) > 1:
+            raise YSDBException("corrupted DB table")
+        return 0    
--- src/ysdb.py
+++ src/ysdb.py
@@ -4,10 +4,29 @@ import argparse
 from db_worker import DbWorkerService
 import logging
 import json
+import time
+from datetime import timedelta, datetime
+from ysdb_exception import YSDBException
+
+    
+def MakeHumanReadableAmount(value:int) -> str: 
+    if value > 1000:
+        return str(round(float(value)/1000.0, 1))+"k" 
+        
+    return str(value)
 
 class YSDBot:
     def __init__(self, db_worker:DbWorkerService):
         self.Db = db_worker
+        self.StartTS = int(time.time())
+        self.LastHandledPushCommand = time.time()
+        self.PushCommandMinimunInterval = 0.8
+        self.LastHandledPopCommand = time.time()
+        self.PopCommandMinimunInterval = 1.5
+        self.LastHandledMyStatCommand = time.time()
+        self.MyStatCommandMinimunInterval = 5
+        self.LastHandledStatCommand = time.time()
+        self.StatCommandMinimunInterval = 10
 
     @staticmethod
     def GetUserTitleForLog(user:User) -> str:
@@ -35,56 +54,134 @@ class YSDBot:
 
     @staticmethod
     def ParsePushMessage(msg:str) -> int:
-        parts = msg.strip().split(" ", 1)
-        second_part = parts[1].strip()
-        koeff = 1
-        if second_part[-1] in ['k', 'K', 'к', 'К']:
-            second_part = second_part[:-1]
-            koeff = 1000
-        return int(second_part)*koeff
+        try:
+            parts = msg.strip().split(" ", 1)
+            second_part = parts[1].strip()
+            koeff = 1
+            if second_part[-1] in ['k', 'K', 'к', 'К']:
+                second_part = second_part[:-1]
+                koeff = 1000
+            return int(second_part)*koeff
+        except BaseException as ex:
+            raise YSDBException("Некорректный формат команды /push")     
 
+
+    def MakeLastPushingInfo(self, user_id:int, chat_id:int, count:int) -> str:
+        user_contribs = self.Db.SelectLastUserSelfContribs(user_id, chat_id, count)
+        result = ""
+        cc = 0
+        for uc in user_contribs:
+            if cc > 0:
+                result += "\n"
+
+            result += "№"+str(cc) +" " + uc.TS.strftime("%d.%m.%Y %H:%M")+": "+MakeHumanReadableAmount(uc.Amount)
+            cc += 1
+
+        return result
+
+    def MakeLastPushingInfoBlock(self, user_id:int, chat_id:int) -> str:
+        result = "Последние 5 записей:\n"
+
+        return result+self.MakeLastPushingInfo(user_id, chat_id, 5)
 
     async def push(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         logging.info("[PUSH] user id "+YSDBot.GetUserTitleForLog(update.effective_user)+", chat id "+YSDBot.GetChatTitleForLog(update.effective_chat) + ", text: "+update.message.text)    
+        if time.time() - self.LastHandledPushCommand < self.PushCommandMinimunInterval:
+            logging.warning("[PUSH] Ignore command from user id "+YSDBot.GetUserTitleForLog(update.effective_user)+", chat id "+YSDBot.GetChatTitleForLog(update.effective_chat) + ", text: "+update.message.text)
+            return
+        self.LastHandledPushCommand = time.time()
 
-        self.Db.EnsureUserExists(update.effective_user.id, YSDBot.MakeUserTitle(update.effective_user))
-        self.Db.EnsureChatExists(update.effective_chat.id, YSDBot.MakeChatTitle(update.effective_chat))
-
-        amount = 0
         try:
-            amount = YSDBot.ParsePushMessage(update.message.text)
-        except BaseException as ex:
-            logging.error("[PUSH] user id "+YSDBot.GetUserTitleForLog(update.effective_user)+", chat id "+YSDBot.GetChatTitleForLog(update.effective_chat) + ", text: "+update.message.text + ". EXCEPTION: "+str(ex))       
+            self.Db.EnsureUserExists(update.effective_user.id, YSDBot.MakeUserTitle(update.effective_user))
+            self.Db.EnsureChatExists(update.effective_chat.id, YSDBot.MakeChatTitle(update.effective_chat))            
+        
+            amount = YSDBot.ParsePushMessage(update.message.text)            
 
-        if amount < 1:
-            await update.message.reply_text("Меньше одного символа пушить нельзя") 
-        if amount > 100000:
-            await update.message.reply_text("Больше 100k пушить нельзя")     
+            if amount < 1:
+                raise YSDBException("Меньше одного символа пушить нельзя") 
+            if amount > 100000:
+                raise YSDBException("Больше 100k пушить нельзя")     
             
-        self.Db.InsertSelfContribRecord(update.effective_user.id, update.effective_chat.id, amount)
+            self.Db.InsertSelfContribRecord(update.effective_user.id, update.effective_chat.id, amount)
 
-        await update.message.reply_text("Сохранено "+str(amount)+" символов. \n\nПоследние 5 записей:") 
+            reply_message = "Сохранено "+MakeHumanReadableAmount(amount)+" символов. \n\n"+self.MakeLastPushingInfoBlock(update.effective_user.id, update.effective_chat.id)
+
+            await update.message.reply_text(reply_message) 
+        except YSDBException as ex:
+            await update.message.reply_text("Ошибка!\n\n"+str(ex)) 
+        except BaseException as ex:    
+            logging.error("[PUSH] user id "+YSDBot.GetUserTitleForLog(update.effective_user)+", chat id "+YSDBot.GetChatTitleForLog(update.effective_chat) + ", text: "+update.message.text + ". EXCEPTION: "+str(ex))       
+            await update.message.reply_text("Ошибка при выполнении команды: "+str(ex))
+    
 
     async def pop(self,update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        logging.info("[POP] user id "+YSDBot.GetUserTitleForLog(update.effective_user)+", chat id "+YSDBot.GetChatTitleForLog(update.effective_chat))    
+        logging.info("[POP] user id "+YSDBot.GetUserTitleForLog(update.effective_user)+", chat id "+YSDBot.GetChatTitleForLog(update.effective_chat)+ ", text: "+update.message.text)    
+        if time.time() - self.LastHandledPopCommand < self.PopCommandMinimunInterval:
+            logging.warning("[POP] Ignore command from user id "+YSDBot.GetUserTitleForLog(update.effective_user)+", chat id "+YSDBot.GetChatTitleForLog(update.effective_chat)+ ", text: "+update.message.text)
+            return
+        self.LastHandledPopCommand = time.time()
+
         await update.message.reply_text(f'pop {update.effective_user.first_name}!')    
 
     async def mystat(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         logging.info("[MYSTAT] user id "+YSDBot.GetUserTitleForLog(update.effective_user)+", chat id "+YSDBot.GetChatTitleForLog(update.effective_chat))    
-        await update.message.reply_text(f'mystat {update.effective_user.first_name}!')    
+        if time.time() - self.LastHandledMyStatCommand < self.MyStatCommandMinimunInterval:
+            logging.warning("[MYSTAT] Ignore command from user id "+YSDBot.GetUserTitleForLog(update.effective_user)+", chat id "+YSDBot.GetChatTitleForLog(update.effective_chat))        
+            return
+        self.LastHandledMyStatCommand = time.time()
+
+        try:
+            stat_message = "Привет, " + YSDBot.MakeUserTitle(update.effective_user) + "!\n\n"
+            stat_message += self.MakeLastPushingInfoBlock(update.effective_user.id, update.effective_chat.id)
+
+            stat_message += "\n"
+            stat_message += "\nЗа последние сутки: "+MakeHumanReadableAmount(self.Db.GetAmountSum(update.effective_user.id, update.effective_chat.id, datetime.now() - timedelta(days=1), datetime.now()))
+            stat_message += "\nЗа последние 3 суток: "+MakeHumanReadableAmount(self.Db.GetAmountSum(update.effective_user.id, update.effective_chat.id, datetime.now() - timedelta(days=3), datetime.now()))
+            stat_message += "\nЗа последние 7 суток: "+MakeHumanReadableAmount(self.Db.GetAmountSum(update.effective_user.id, update.effective_chat.id, datetime.now() - timedelta(days=7), datetime.now()))
+            stat_message += "\nЗа последние 15 суток: "+MakeHumanReadableAmount(self.Db.GetAmountSum(update.effective_user.id, update.effective_chat.id, datetime.now() - timedelta(days=15), datetime.now()))
+            stat_message += "\nЗа последние 30 суток: "+MakeHumanReadableAmount(self.Db.GetAmountSum(update.effective_user.id, update.effective_chat.id, datetime.now() - timedelta(days=30), datetime.now()))
+            stat_message += "\nЗа всё время: "+MakeHumanReadableAmount(self.Db.GetAmountSum(update.effective_user.id, update.effective_chat.id, datetime.now() - timedelta(days=3600), datetime.now()))
+
+            if update.effective_user.id == update.effective_chat.id:
+                stat_message += "\n\n((Тут будет статистика по всем чатам))"
+
+            await update.message.reply_text(stat_message)     
+        except YSDBException as ex:
+            await update.message.reply_text("Ошибка!\n\n"+str(ex)) 
+        except BaseException as ex:    
+            logging.error("[MYSTAT] user id "+YSDBot.GetUserTitleForLog(update.effective_user)+", chat id "+YSDBot.GetChatTitleForLog(update.effective_chat) + ", text: "+update.message.text + ". EXCEPTION: "+str(ex))       
+            await update.message.reply_text("Ошибка при выполнении команды: "+str(ex))  
 
     async def stat(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:        
         logging.info("[STAT] user id "+YSDBot.GetUserTitleForLog(update.effective_user)+", chat id "+YSDBot.GetChatTitleForLog(update.effective_chat))    
+        if time.time() - self.LastHandledStatCommand < self.StatCommandMinimunInterval:
+            logging.warning("[STAT] Ignore command from user id "+YSDBot.GetUserTitleForLog(update.effective_user)+", chat id "+YSDBot.GetChatTitleForLog(update.effective_chat))                
+            return
+        self.LastHandledStatCommand = time.time()
 
-        await update.message.reply_text(f'stat {update.effective_user.first_name}!')     
+        try:
+            stat_message = "Это чат " + YSDBot.MakeChatTitle(update.effective_chat) + "\n\n"
 
-    async def status(self,update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        logging.info("[STATUS] user id "+YSDBot.GetUserTitleForLog(update.effective_user)+", chat id "+YSDBot.GetChatTitleForLog(update.effective_chat))    
+                     
 
-        await update.message.reply_text(f'Hello {update.effective_user.first_name}!')
+            await update.message.reply_text(stat_message)     
+        except YSDBException as ex:
+            await update.message.reply_text("Ошибка!\n\n"+str(ex)) 
+        except BaseException as ex:    
+            logging.error("[MYSTAT] user id "+YSDBot.GetUserTitleForLog(update.effective_user)+", chat id "+YSDBot.GetChatTitleForLog(update.effective_chat) + ", text: "+update.message.text + ". EXCEPTION: "+str(ex))       
+            await update.message.reply_text("Ошибка при выполнении команды: "+str(ex))  
+       
 
-
-
+    async def status(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        ut = YSDBot.GetUserTitleForLog(update.effective_user)
+        logging.info("[STATUS] user id "+ut+", chat id "+YSDBot.GetChatTitleForLog(update.effective_chat))    
+        status_msg = "Привет, "+YSDBot.MakeUserTitle(update.effective_user)+"! ("+ut+")"
+        status_msg +="\nЭто чат: "+YSDBot.MakeChatTitle(update.effective_chat)
+        uptime_sec = time.time() - self.StartTS
+        uptime = timedelta(seconds = uptime_sec)
+        status_msg +="\nАптайм "+ str(uptime)
+        #status_msg +="\nВерсия "+ str(uptime)
+        await update.message.reply_text(status_msg)
 
 
 if __name__ == '__main__':
